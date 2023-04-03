623de7134a0c09deb4adb17f9573d952b0770872
bhaskarmehta <46514777+bhaskarmehta@users.noreply.github.com>
2023-04-03 15:43:27 +0530

    test

--- get_nodegroup_name.py
+++ get_nodegroup_name.py
@@ -2,11 +2,12 @@ import subprocess
 import json
 
 # Set the name of your EKS cluster and nodegroup
-cluster_name = 'eks-test23'
+cluster_name = 'my-eks-cluster'
+nodegroup_name = 'my-nodegroup'
 region = 'ap-south-1'
 
 # Get the ARN of the nodegroup
-cmd = f'aws eks describe-nodegroup --cluster-name {cluster_name} --region {region}'
+cmd = f'aws eks describe-nodegroup --cluster-name {cluster_name} --nodegroup-name {nodegroup_name} --region {region}'
 output = subprocess.check_output(cmd, shell=True)
 nodegroup_arn = json.loads(output.decode('utf-8'))['nodegroup']['nodegroupArn']
 
@@ -14,3 +15,4 @@ nodegroup_arn = json.loads(output.decode('utf-8'))['nodegroup']['nodegroupArn']
 nodegroup_name = nodegroup_arn.split('/')[-1]
 
 print(f'The nodegroup name for {cluster_name} in {region} is {nodegroup_name}')
+
