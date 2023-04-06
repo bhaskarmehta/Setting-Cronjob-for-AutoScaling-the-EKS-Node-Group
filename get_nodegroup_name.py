612071860fd162e52e448902857051b92d21749a
bhaskarmehta <46514777+bhaskarmehta@users.noreply.github.com>
2023-04-06 11:08:05 +0530

    test

--- get_nodegroup_name.py
+++ get_nodegroup_name.py
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     import datetime
     
     now = datetime.datetime.now()
-    if now.hour == 11:
+    if now.hour == 16:
         scale_up()
     elif now.hour == 20:
         scale_down()
